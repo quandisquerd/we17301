4c3cea070256dc563fba50629894cf0bcef8a514
quan1234 <quanvdph19781@fpt.edu.vn>
2023-01-30 20:40:11 +0700

    ok

--- chailen1.js
+++ chailen1.js
@@ -36,3 +36,22 @@ const game = {
 
 const [players1,players2]=game.players
 console.log(players1,players2);
+const [gk,...fieldPlayers] = players1;
+console.log(gk,fieldPlayers);
+const allPlayers =[...players1,...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1,'Thiago','Coutino','Periscic'];
+
+const {odds:{team1,x:draw,team2}}=game;
+console.log(team1,draw,team2);
+
+const printGoal=function(...players){
+    console.log(`${players.length}`)
+}
+printGoal('Davies','Muller','Lewandowski','Kimmich');
+printGoal('Davies','Muller','Lewandowski','Kimmich');
+printGoal(...game.scored);
+
+team1 < team2 && console.log('team 1 is more likelu to win');
+team1 < team2 && console.log('team 1 is more likelu to win')
